a130df0be94cd9f0b88e303c139113d34d2972c1
开发部2 软件 <rjkfb2@ruanjiankaifabu2deimac.local>
2016-05-16 09:04:24 +0800

    Initial Commit

---
+++ PFImagePickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PFImagePickerController
+//
+//  Created by 软件开发部2 on 16/5/16.
+//  Copyright © 2016年 软件开发部2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PFImagePickerController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PFImagePickerController
+//
+//  Created by 软件开发部2 on 16/5/16.
+//  Copyright © 2016年 软件开发部2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
